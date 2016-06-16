e957adb3693d82519d5bfb1c49b62878ee2f3f4f
Hakeem Oriola <hakeem.oriola@oryx-wa.com>
2016-06-16 17:34:03 +0100

    MCIData Validations Added

--- src/OryxMCI.Data/OryxDataValidation.cs
+++ src/OryxMCI.Data/OryxDataValidation.cs
@@ -1,6 +1,6 @@
-﻿using System;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using OryxMCI.Entities;
+using System;
 
 namespace OryxMCI.Data
 {
@@ -33,10 +33,34 @@ namespace OryxMCI.Data
             builder.Entity<Vessel>().Property(m => m.Code).IsRequired().HasMaxLength(10);
             builder.Entity<Vessel>().Property(m => m.Name).IsRequired().HasMaxLength(100);
 
-            // Berth
-            builder.Entity<Berth>().HasKey(m => m.Id);
-            builder.Entity<Berth>().Property(m => m.Code).IsRequired().HasMaxLength(10);
-            builder.Entity<Berth>().Property(m => m.Name).IsRequired().HasMaxLength(100);
+            // MCIData
+            builder.Entity<MCIData>().HasKey(m => m.Id);
+            builder.Entity<MCIData>().Property(m => m.MCINumber).IsRequired();
+            builder.Entity<MCIData>().Property(m => m.PortId).IsRequired();
+            builder.Entity<MCIData>().Property(m => m.Port).IsRequired().HasMaxLength(100);
+            builder.Entity<MCIData>().Property(m => m.VesselId).IsRequired();
+            builder.Entity<MCIData>().Property(m => m.VesselName).IsRequired().HasMaxLength(100);
+            builder.Entity<MCIData>().Property(m => m.GrossTonnage).IsRequired();
+            builder.Entity<MCIData>().Property(m => m.CargoTonnage).IsRequired();
+            builder.Entity<MCIData>().Property(m => m.CargoOut).IsRequired();
+            builder.Entity<MCIData>().Property(m => m.AgentId).IsRequired();
+            builder.Entity<MCIData>().Property(m => m.Agent).IsRequired().HasMaxLength(100);
+            builder.Entity<MCIData>().Property(m => m.WasteCollectionNoteYear).IsRequired().HasMaxLength(4);
+            builder.Entity<MCIData>().Property(m => m.WasteCollectionNoteNo).IsRequired().HasMaxLength(20);
+            builder.Entity<MCIData>().Property(m => m.WasteCollectionNote).IsRequired().HasMaxLength(255);
+            builder.Entity<MCIData>().Property(m => m.WasteTransferNoteOil).IsRequired();
+            builder.Entity<MCIData>().Property(m => m.WasteTransferNoteGarbage).IsRequired().HasMaxLength(255);
+            builder.Entity<MCIData>().Property(m => m.Waste).IsRequired();
+            builder.Entity<MCIData>().Property(m => m.Type).IsRequired().HasMaxLength(20);
+            builder.Entity<MCIData>().Property(m => m.Archive).IsRequired();
+            builder.Entity<MCIData>().Property(m => m.NEXTWTNNO).IsRequired().HasMaxLength(20);
+            builder.Entity<MCIData>().Property(m => m.InspectorId).IsRequired();
+            builder.Entity<MCIData>().Property(m => m.Inspector).IsRequired().HasMaxLength(100);
+            builder.Entity<MCIData>().Property(m => m.MissingReason).IsRequired().HasMaxLength(255);
+            builder.Entity<MCIData>().Property(m => m.Notes).IsRequired().HasMaxLength(255);
+            builder.Entity<MCIData>().Property(m => m.GarbageQty).IsRequired().HasMaxLength(20);
+            builder.Entity<MCIData>().Property(m => m.OilQty).IsRequired().HasMaxLength(20);
+            builder.Entity<MCIData>().Property(m => m.Processed).IsRequired().HasMaxLength(1);
         }
     }
 }
