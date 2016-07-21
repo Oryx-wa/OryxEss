118c6261e0b6db60b7c6629c996b6b58be6c584c
Hakeem Oriola <hakeem.oriola@oryx-wa.com>
2016-07-21 13:34:50 +0100

    ok

--- src/OryxMCI_web/src/app/agents/agents.component.ts
+++ src/OryxMCI_web/src/app/agents/agents.component.ts
@@ -1,75 +1,73 @@
 import { Component, OnInit } from '@angular/core';
 import { CORE_DIRECTIVES } from '@angular/common';
- import { Router, ActivatedRoute,  ROUTER_DIRECTIVES } from '@angular/router';
- import {MdIcon, MdIconRegistry} from '@angular2-material/icon'; 
- import {MD_BUTTON_DIRECTIVES} from '@angular2-material/button';
- import {MD_TOOLBAR_DIRECTIVES} from '@angular2-material/toolbar';
- import { AuthGuard }             from '../login/auth.guard';
- import {AgentsListComponent} from './agents-list.component';
+import { Router, ActivatedRoute, ROUTER_DIRECTIVES } from '@angular/router';
+import {MdIcon, MdIconRegistry} from '@angular2-material/icon';
+import {MD_BUTTON_DIRECTIVES} from '@angular2-material/button';
+import {MD_TOOLBAR_DIRECTIVES} from '@angular2-material/toolbar';
+import { AuthGuard }             from '../login/auth.guard';
+import {AgentsListComponent} from './agents-list.component';
 
- import {ServiceBase} from '../shared/service-base';
- import {IModelBase} from '../shared/imodel-base';
- import {SecurityService} from '../login/security.service';
+import {ServiceBase} from '../shared/service-base';
+import {IModelBase} from '../shared/imodel-base';
+import {SecurityService} from '../login/security.service';
 
 @Component({
   moduleId: module.id,
   selector: 'app-agents',
   templateUrl: 'agents.component.html',
   styleUrls: ['agents.component.css'],
-   directives: [MD_TOOLBAR_DIRECTIVES, MD_BUTTON_DIRECTIVES, 
-   MdIcon, ROUTER_DIRECTIVES, AgentsListComponent, CORE_DIRECTIVES],
-    providers: [MdIconRegistry]
+  directives: [MD_TOOLBAR_DIRECTIVES, MD_BUTTON_DIRECTIVES,
+    MdIcon, ROUTER_DIRECTIVES, AgentsListComponent, CORE_DIRECTIVES],
+  providers: [MdIconRegistry]
 })
 export class AgentsComponent implements OnInit {
-title: string;
-    filterText: string;
-    agents: IModelBase[] = [];
-    agent: IModelBase;
-    //filteredCustomers: ICustomer[] = [];
-    displayMode: DisplayModeEnum;
-    displayModeEnum = DisplayModeEnum;
-  constructor(private _AgentService: ServiceBase, public securityService: SecurityService) {}
+  title: string;
+  filterText: string;
+  agents: IModelBase[] = [];
+  agent: IModelBase;
+  //filteredCustomers: ICustomer[] = [];
+  displayMode: DisplayModeEnum;
+  displayModeEnum = DisplayModeEnum;
+  constructor(private _AgentService: ServiceBase, public securityService: SecurityService) { }
 
   ngOnInit() {
     this._AgentService.setActionUrl('agent');
-      
+
     this.displayMode = DisplayModeEnum.Grid;
     this.getData();
   }
-changeDisplayMode(mode: DisplayModeEnum) {
+  changeDisplayMode(mode: DisplayModeEnum) {
     switch (mode) {
       case DisplayModeEnum.Details:
         this.getDataById(1);
         break;
-    
+
       default:
         break;
     }
-      this.displayMode = mode;
-    }
+    this.displayMode = mode;
+  }
 
-    private getData() {
-        console.log('agent :getData starting...');
-        this._AgentService
-            .GetAll()
-            .subscribe(data => this.agents = data,
-            error => this.securityService.HandleError(error),
-            () => console.log('Get all completed'));
-          console.log(this.agents.length.toString());
-    }
+  private getData() {
+    console.log('agent :getData starting...');
+    this._AgentService
+      .GetAll()
+      .subscribe(data => this.agents = data,
+      error => this.securityService.HandleError(error),
+      () => console.log('Get all completed'));
+    console.log(this.agents.length.toString());
+  }
 
-     private getDataById(id: number) {
-        
-           console.log('iou :getData starting...');
-          this._AgentService
-            .GetById(id)
-            .subscribe(data => this.agent = data,
-            error => this.securityService.HandleError(error),
-            () => console.log('Get id completed'));           
-          console.log("id completed successfully");
-        
-  
+  private getDataById(id: number) {
 
+    console.log('iou :getData starting...');
+    this._AgentService
+      .GetById(id)
+      .subscribe(data => this.agent = data,
+      error => this.securityService.HandleError(error),
+      () => console.log('Get id completed'));
+    console.log("id completed successfully");
+  }
 }
 }
 
