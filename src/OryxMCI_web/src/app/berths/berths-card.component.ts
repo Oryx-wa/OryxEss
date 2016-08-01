5da2d58bfec690f4281b6131062c321e151ece22
BabatundeAlo <babatunde.olabode@oryx-wa.com>
2016-08-02 02:52:37 +0800

    vessel and berth card done with home

--- src/OryxMCI_web/src/app/berths/berths-card.component.ts
+++ src/OryxMCI_web/src/app/berths/berths-card.component.ts
@@ -1,16 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Router, ActivatedRoute,  ROUTER_DIRECTIVES } from '@angular/router';
+import { CORE_DIRECTIVES } from '@angular/common';
+import { FORM_DIRECTIVES,NgForm } from '@angular/forms';
+
+import {MdIcon, MdIconRegistry} from '@angular2-material/icon'; 
+import {MD_BUTTON_DIRECTIVES} from '@angular2-material/button';
+import {MD_TOOLBAR_DIRECTIVES} from '@angular2-material/toolbar';
+import { AuthGuard }             from '../login/auth.guard';
+import {MaterializeDirective} from "angular2-materialize";
+import {PaginatePipe, PaginationControlsCmp} from 'ng2-pagination';
+import {ServiceBase} from '../shared/service-base';
+ import {IModelBase} from '../shared/imodel-base';
 
 @Component({
   moduleId: module.id,
   selector: 'app-berths-card',
   templateUrl: 'berths-card.component.html',
-  styleUrls: ['berths-card.component.css']
+  styleUrls: ['berths-card.component.css'],
+   directives: [MD_TOOLBAR_DIRECTIVES, MD_BUTTON_DIRECTIVES,CORE_DIRECTIVES, PaginationControlsCmp, MaterializeDirective,
+  MdIcon, ROUTER_DIRECTIVES, FORM_DIRECTIVES ],
+    providers: [MdIconRegistry],
+    pipes: [PaginatePipe],
+    
 })
 export class BerthsCardComponent implements OnInit {
+  @Input() berths: any[] = [];
+  berth: IModelBase;
+  formActive:Boolean = false;
+  constructor() {};
 
-  constructor() {}
 
   ngOnInit() {
   }
 
+  showForm(id:number){
+     this.berth = this.berths.find(t => t.id == id);
+    this.formActive = true;
+    console.log(id);
+    
+  }
+
+  saveData(){
+    console.log(JSON.stringify(this.berth));
+  }
+
 }
+
+
+  
