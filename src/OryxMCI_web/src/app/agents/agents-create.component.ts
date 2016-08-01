6a8f96d7a136b2bcf1cc880132e15dd9ddece64f
Hakeem Oriola <hakeem.oriola@oryx-wa.com>
2016-08-01 19:52:25 +0100

    toolbar fixed

--- src/OryxMCI_web/src/app/agents/agents-create.component.ts
+++ src/OryxMCI_web/src/app/agents/agents-create.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import {IModelBase} from '../shared/imodel-base';
 
 @Component({
   moduleId: module.id,
@@ -7,8 +8,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['agents-create.component.css']
 })
 export class AgentsCreateComponent implements OnInit {
-
-  constructor() {}
+  @Input() agent: IModelBase;
+  formActive: Boolean = false;
+  selectedId: number = 0;
+  constructor() { }
 
   ngOnInit() {
   }
