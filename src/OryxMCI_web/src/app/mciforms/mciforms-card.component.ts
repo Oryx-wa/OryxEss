2383e5cb8572d53db90000ac86edf7ea7577ad97
adeluola <bimbodelu@gmail.com>
2016-08-02 09:57:06 +0100

    worked on mciform card and port card

--- src/OryxMCI_web/src/app/mciforms/mciforms-card.component.ts
+++ src/OryxMCI_web/src/app/mciforms/mciforms-card.component.ts
@@ -1,16 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+
+
+import { Component, OnInit, Input } from '@angular/core';
+import { Router, ActivatedRoute,  ROUTER_DIRECTIVES } from '@angular/router';
+import { CORE_DIRECTIVES } from '@angular/common';
+import { FORM_DIRECTIVES,NgForm } from '@angular/forms';
+
+import {MdIcon, MdIconRegistry} from '@angular2-material/icon'; 
+import {MD_BUTTON_DIRECTIVES} from '@angular2-material/button';
+import {MD_TOOLBAR_DIRECTIVES} from '@angular2-material/toolbar';
+import { AuthGuard }             from '../login/auth.guard';
+import {MaterializeDirective} from "angular2-materialize";
+import {PaginatePipe, PaginationControlsCmp} from 'ng2-pagination';
+import {ServiceBase} from '../shared/service-base';
+ import {IModelBase} from '../shared/imodel-base';
 
 @Component({
   moduleId: module.id,
   selector: 'app-mciforms-card',
   templateUrl: 'mciforms-card.component.html',
-  styleUrls: ['mciforms-card.component.css']
+  styleUrls: ['mciforms-card.component.css'],
+  directives: [MD_TOOLBAR_DIRECTIVES, MD_BUTTON_DIRECTIVES,CORE_DIRECTIVES, PaginationControlsCmp, MaterializeDirective,
+  MdIcon, ROUTER_DIRECTIVES, FORM_DIRECTIVES ],
+    providers: [MdIconRegistry],
+    pipes: [PaginatePipe]
 })
 export class MciFormsCardComponent implements OnInit {
+@Input() mciforms: any[] = [];
+  mciform: IModelBase;
+  formActive:Boolean = false;
+  constructor() {};
 
-  constructor() {}
 
   ngOnInit() {
+  }
+
+  showForm(id:number){
+     this.mciform = this.mciforms.find(t => t.id == id);
+    this.formActive = true;
+    console.log(id);
+    
+  }
+
+  saveData(){
+    console.log(JSON.stringify(this.mciform));
   }
 
 }
