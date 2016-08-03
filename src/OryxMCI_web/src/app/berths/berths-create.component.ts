9034cdf359b47a98420c8aafbf794aeb30e791a0
BabatundeAlo <babatunde.olabode@oryx-wa.com>
2016-08-04 02:29:52 +0800

    berth create fixed

--- src/OryxMCI_web/src/app/berths/berths-create.component.ts
+++ src/OryxMCI_web/src/app/berths/berths-create.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { CORE_DIRECTIVES  } from '@angular/common';
+import { FORM_DIRECTIVES,NgForm } from '@angular/forms';
+import {MaterializeDirective} from "angular2-materialize";
+import {IModelBase} from '../shared/imodel-base';
+import {IBerth, Berth} from './berth';
 
 @Component({
   moduleId: module.id,
@@ -7,10 +12,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['berths-create.component.css']
 })
 export class BerthsCreateComponent implements OnInit {
+berth: Berth;
+  formActive: Boolean = false;
+  selectedId: number = 0;
+  @Input() berths:any[] = [];
 
   constructor() {}
 
   ngOnInit() {
+     this.berth = new Berth( );
+    this.berth.id= 1;
   }
 
 }
